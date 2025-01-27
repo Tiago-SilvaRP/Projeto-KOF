94df7d037e363ca66cb86201cfb4010ea52646a1
Tiago Silva <tiagosilva_live_2016@outlook.com>
2025-01-26 22:04:24 -0300

    criado evento de carta virada

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,8 +1,16 @@
 const botaoAvancar = document.getElementById("btn-avancar")
 const botaoVoltar = document.getElementById("btn-voltar")
 const cartoes = document.querySelectorAll(".cartao")
-
 let cartaoAtual = 0
+
+cartoes.forEach(cartao => {
+    cartao.addEventListener('click', () => {
+        
+        virarEMostrarFundoCarta(cartao)
+        esconderDescricaoCartao(cartao)
+    })
+    
+})
 
 botaoAvancar.addEventListener("click", () => {
     if (cartaoAtual >= cartoes.length -1) {
@@ -26,6 +34,17 @@ botaoVoltar.addEventListener("click", () => {
     mostrarCartao(cartaoAtual);
 });
 
+function virarEMostrarFundoCarta(cartao) {
+    const cartaVirada = cartao.querySelector('.carta-virada')
+    cartao.classList.toggle("virar")
+    cartaVirada.classList.toggle('mostrar-fundo-carta')
+}
+
+function esconderDescricaoCartao(cartao) {
+    const descricao = cartao.querySelector('.descricao')
+    descricao.classList.toggle('esconder')
+}
+
 function mostrarCartao(cartaoAtual) {
     cartoes[cartaoAtual].classList.add("selecionado");
 }
@@ -34,3 +53,4 @@ function esconderCartaoSelecionado() {
     const cartaoSelecionado = document.querySelector(".selecionado");
     cartaoSelecionado.classList.remove("selecionado");
 }
+
